87b6dce0c179f94ead83fef9646c6cf7fbe65a48
DinhNhat <nguyendinhnhat206@gmail.com>
2022-11-30 10:59:41 -0500

    -- Initial commit.

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Hello {{ name }}</h1>
+    <p>In this tutorial we will use the series frameworks</p>
+
+    <Nav />
+</template>
+
+<script>
+import Nav from '../Shared/Nav.vue';
+
+export default {
+    components: { Nav },
+}
+</script>
